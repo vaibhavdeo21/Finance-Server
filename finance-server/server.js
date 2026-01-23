a0120c717fcf4ef7238a0d524d119304ede8dbf2
Vaibhav Pratap Deo <vaibhavpdeo0028@gmail.com>
2026-01-23 11:52:10 +0530

    modified server.js to add a user

--- finance-server/server.js
+++ finance-server/server.js
@@ -1,1 +1,35 @@
-console.log("Hi, I am your server!");
+const express = require("express");
+
+const app = express(); // Creates javascript object named app
+
+app.use(express.json()); // Middleware (converts JSON into javascript objects)
+
+let users = []; // Creating empty array of users so that whenevr new user is created it gets stored ove here
+
+app.post('/register', (request, response) =>{ //if request coming matches with /register then this will get executed
+    const {name, email, password} = request.body;
+    
+    if(!name || !email || !password){
+        return response.status(400).json({
+            message: 'Name, Email, Password are required'
+        });
+    }
+
+    const newUser = {
+        id: users.length + 1,
+        name: name,
+        email: email,
+        password: password
+    };
+
+    users.push(newUser);
+
+    return response.status(200).json({
+        message: "User Registered Successfully!",
+        user: { id: newUser.id }
+    });
+});
+
+app.listen(3001, () => {
+    console.log("Server is running on port 3001");
+});
