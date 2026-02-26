6026f1bc000c35a99d687df42d9d27984fa65b38
vaibhavdeo21 <vaibhavpdeo0028@gmail.com>
2026-02-26 11:48:20 +0530

    minor change

--- src/dao/rbacDao.js
+++ src/dao/rbacDao.js
@@ -12,6 +12,7 @@ const rbacDao = {
     },
 
     update: async (userId, name, role) => {
+        return await User.findByIdAndUpdate(
             userId,
             { name, role },
             { new: true }
